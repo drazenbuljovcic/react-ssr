f968a02cbc0edfe67fa41202fb0740305444835e
Dražen Buljovčić <dbuljovcic@execom.eu>
2017-01-17 14:06:22 +0100

    Deleted unnecessary files

--- src/app/vendor.js
+++ src/app/vendor.js
@@ -1,6 +1,3 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'react-router';
-
-import { applyMiddleware, compose, createStore} from 'redux';
-import logger from 'redux-logger';
